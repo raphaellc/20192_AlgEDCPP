f2474afeeb851eee8f5f921b0a666dcff5f23fec
Patricia Silva <51720805+Patiinhasilvva@users.noreply.github.com>
2019-08-20 22:18:03 -0300

    Adicionando coisas ao repositório

---
+++ Patricia/20.08.2/20.08.2/Origem.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "VetorDinamico.h"
+
+
+/*Regras
+->Iniciar xom 5 valores alocados
+->Realoca o vetor de 5 em 5 elementos ( remocao e adicao)
+->Testar a classe no main
+*/
---
+++ Patricia/20.08.2/20.08.2/VetorDinamico.cpp
@@ -0,0 +1,28 @@
+#include "VetorDinamico.h"
+
+
+
+VetorDinamico::VetorDinamico()
+{
+}
+
+
+VetorDinamico::~VetorDinamico()
+{
+
+}
+
+void VetorDinamico::adiciona_valor(int valor)
+{
+
+}
+
+int VetorDinamico::obtem_valor(int posicao)
+{
+	return 0;
+}
+
+int VetorDinamico::remove_valor(int posicao)
+{
+	return 0;
+}
