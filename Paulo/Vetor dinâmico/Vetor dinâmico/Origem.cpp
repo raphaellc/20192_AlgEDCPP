762824cab787a8edd8d315c6c14a167e395bcbf6
UltraGaren <43190093+UltraGaren@users.noreply.github.com>
2019-08-22 21:21:08 -0300

    resolvi uns problemas e tal

--- Paulo/Vetor dinâmico/Vetor dinâmico/Origem.cpp
+++ Paulo/Vetor dinâmico/Vetor dinâmico/Origem.cpp
@@ -10,27 +10,26 @@ int main() {
 	//Realoca o vetor de 5 em 5 elementos (remocao e adicao)
 	//Testa a classe no Main
 
-	int aux, num;
+	int aux, num = 0;
 	srand(time(0));
 
 	VetorDinamico * vetor = new VetorDinamico();
-
-	for (int i = 0; i < 8; i++) {
+	
+	for (int i = 0; i < 18; i++) {
 		aux = rand() % 10;
 		vetor->adiciona_valor(aux);
+		cout << "[" << i << "]" << vetor->obtem_valor(i) << "\t" << "Tamanho vetor: " << vetor->tamanho << endl;
 	}
 
-	for (int i = 0; i < 8; i++) {
-		cout << vetor->obtem_valor(i) << endl;
-	}
+	cout << "Digite -1 para parar de remover valores\n";
 
-	cout << "Digite um numero para remover: ";
-	cin >> num;
-
-	vetor->remove_valor(num);
-
-	for (int i = 0; i < 7; i++) {
-		cout << vetor->obtem_valor(i) << endl;
+	while (num != -1 && vetor->contador) {
+		cout << endl << "Digite uma posicao para remover: ";
+		cin >> num;
+		vetor->remove_valor(num);
+		for (int i = 0; i < vetor->contador; i++) {
+			cout << "[" << i << "]" << vetor->obtem_valor(i) << "\t" << "Tamanho vetor: " << vetor->tamanho << endl;
+		}
 	}
 
 	system("pause");
