07b293ab2686821965474713c4e0b8657bbe757b
UltraGaren <43190093+UltraGaren@users.noreply.github.com>
2019-09-05 17:15:39 -0300

    aaa não troquei de branch

---
+++ Paulo/Lista2_Ex2/Exercicio2/Exercicio2/Retangulo.cpp
@@ -0,0 +1,31 @@
+#include "Retangulo.h"
+
+
+
+Retangulo::Retangulo()
+{
+}
+
+
+Retangulo::~Retangulo()
+{
+}
+
+void Retangulo::calcularAltura() {
+}
+
+void Retangulo::calcularComprimento() {
+}
+
+void Retangulo::calcularArea() {
+}
+
+void Retangulo::calcularPerimetro() {
+}
+
+void Retangulo::setCoordenadas(float *ponto){
+
+
+
+
+}
