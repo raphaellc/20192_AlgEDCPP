4296ed5085323bbd4d37952938685e7afbdd43d4
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2019-09-17 20:47:08 -0300

    ArquivosBinarios

---
+++ Raphael/ExemplosAula/ArquivoBinario/ArquivoBinario/Origem.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+int * carregaInteiros() {
+	int * vetorInt = new int[10];
+	ifstream if_arquivo;
+	if_arquivo.open("Dados.bin", ios::binary);
+	if (!if_arquivo.is_open())
+		return 0;
+	int j = 0;
+	while (!if_arquivo.eof() && j < 10) {
+		//cout << if_arquivo.eof() << " ";
+		if_arquivo.read(reinterpret_cast<char *>(&vetorInt[j]), sizeof(int));
+		cout << vetorInt[j] << " ";
+		j++;
+		//cout << if_arquivo.eof() << " ";
+	}
+	return vetorInt;
+}
+void main() {
+	ofstream arquivo;//("Dados.bin", ios::binary | ios::out);
+	arquivo.open("Dados.bin", ios::binary);
+	if (!arquivo.is_open()) {
+		cout << "Arquivo não abriu" << endl;
+		exit(1);
+	}
+	
+	for (int i = 0; i < 10; i++) {
+
+		arquivo.write(reinterpret_cast<char *>(&i), sizeof(int));
+		//cout << i << endl;
+	}
+	arquivo.close();
+	carregaInteiros();
+	system("pause");
+}
